087c838b2e3fae8f222e18c0a37cbd76db396294
M7MDXD3GG <mohammed.sheblaq@gmail.com>
2025-09-08 15:56:54 +0300

    New

--- Test.py
+++ Test.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 print("Hello torld")
 print("Hello torld")
 print("Hello torld")
-=======
-print("Hello sorld")
-print("Hello sorld")
-print("Hello sorld")
->>>>>>> 414148e34aa88c5755f4013841268c5d5535e3ea
